c8c7bbe8b9179bff889d59d53786e4584523d4e2
Atsushi Watanabe <atsushi.w@ieee.org>
2016-01-25 16:46:02 +0900

    obj_to_pointcloud: multi-file input (comma separated)

--- src/obj_to_pointcloud.cpp
+++ src/obj_to_pointcloud.cpp
@@ -11,9 +11,13 @@
 #include <pcl/conversions.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/io/vtk_lib_io.h>
+#include <pcl/filters/voxel_grid.h>
 
 #include <cmath>
 #include <random>
+#include <string>
+#include <iostream>
+#include <sstream>
 
 pcl::PointXYZ operator-(const pcl::PointXYZ &a, const pcl::PointXYZ &b)
 {
@@ -40,6 +44,18 @@ pcl::PointXYZ operator*(const pcl::PointXYZ &a, const float &b)
 	return c;
 }
 
+std::vector<std::string> split(const std::string& input, char delimiter)
+{
+	std::istringstream stream(input);
+
+	std::string field;
+	std::vector<std::string> result;
+	while (std::getline(stream, field, delimiter)) {
+		result.push_back(field);
+	}
+	return result;
+}
+
 class obj_to_pointcloud
 {
 public:
@@ -47,19 +63,20 @@ public:
 		n("~"),
 		engine(seed_gen())
 	{
-		pubCloud = n.advertise<sensor_msgs::PointCloud2>("clouds", 1, true);
+		pubCloud = n.advertise<sensor_msgs::PointCloud2>("cloud", 1, true);
 
 		n.param("frame_id", frame_id, std::string("map"));
-		n.param("obj", file, std::string(""));
+		n.param("objs", file, std::string(""));
 		if(file.compare("") == 0)
 		{
 			ROS_ERROR("OBJ file not specified");
 			ros::shutdown();
 			return;
 		}
-		n.param("points_per_meter_sq", ppmsq, 100.0);
+		n.param("points_per_meter_sq", ppmsq, 600.0);
+		n.param("downsample_grid", downsample_grid, 0.05);
 
-		auto pc = convert_obj(file);
+		auto pc = convert_obj(split(file, ','));
 		pubCloud.publish(pc);
 	}
 private:
@@ -69,73 +86,85 @@ private:
 	std::string file;
 	std::string frame_id;
 	double ppmsq;
+	double downsample_grid;
 
 	std::random_device seed_gen;
 	std::default_random_engine engine;
 
-	sensor_msgs::PointCloud2 convert_obj(const std::string &file)
+	sensor_msgs::PointCloud2 convert_obj(const std::vector<std::string> &files)
 	{
 		sensor_msgs::PointCloud2 pc_msg;
 		pcl::PolygonMesh::Ptr mesh(new pcl::PolygonMesh());
 		pcl::PointCloud<pcl::PointXYZ>::Ptr pc(new pcl::PointCloud<pcl::PointXYZ>());
 		pcl::PointCloud<pcl::PointXYZ>::Ptr pc_rs(new pcl::PointCloud<pcl::PointXYZ>());
 
-		if(pcl::io::loadPolygonFileOBJ(file, *mesh) == -1)
+		for(auto &file: files)
 		{
-			ROS_ERROR("Failed to load OBJ file");
-			ros::shutdown();
-			return pc_msg;
-		}
-
-		pcl::fromPCLPointCloud2(mesh->cloud, *pc);
-		pc_rs->header = pc->header;
-
-		std::uniform_real_distribution<float> ud(0.0, 1.0);
-		for(auto &poly: mesh->polygons)
-		{
-			if(poly.vertices.size() != 3)
+			if(pcl::io::loadPolygonFileOBJ(file, *mesh) == -1)
 			{
-				ROS_ERROR("Input mesh mush be triangle");
+				ROS_ERROR("Failed to load OBJ file");
 				ros::shutdown();
 				return pc_msg;
 			}
-			auto &p0 = pc->points[poly.vertices[0]];
-			auto &p1 = pc->points[poly.vertices[1]];
-			auto &p2 = pc->points[poly.vertices[2]];
 
-			auto a = p1 - p0;
-			auto b = p2 - p0;
+			pcl::fromPCLPointCloud2(mesh->cloud, *pc);
+			pc_rs->header = pc->header;
 
-			float s = 0.5 * sqrtf(
-					powf(a.y * b.z - a.z * b.y, 2.0) + 
-					powf(a.z * b.x - a.x * b.z, 2.0) + 
-					powf(a.x * b.y - a.y * b.x, 2.0)
-					);
-			float numf = ppmsq * s;
-			int num = numf;
-			if(numf - num > ud(engine)) num ++;
-			for(int i = 0; i < num; i ++)
+			std::uniform_real_distribution<float> ud(0.0, 1.0);
+			for(auto &poly: mesh->polygons)
 			{
-				float r0 = ud(engine);
-				float r1 = ud(engine);
-				if(r0 + r1 > 1.0)
+				if(poly.vertices.size() != 3)
 				{
-					r0 = 1.0 - r0;
-					r1 = 1.0 - r1;
+					ROS_ERROR("Input mesh mush be triangle");
+					ros::shutdown();
+					return pc_msg;
 				}
-				pcl::PointXYZ p = p0 + (a * r0 + b * r1);
-				pc_rs->points.push_back(p);
+				auto &p0 = pc->points[poly.vertices[0]];
+				auto &p1 = pc->points[poly.vertices[1]];
+				auto &p2 = pc->points[poly.vertices[2]];
+
+				auto a = p1 - p0;
+				auto b = p2 - p0;
+
+				float s = 0.5 * sqrtf(
+						powf(a.y * b.z - a.z * b.y, 2.0) + 
+						powf(a.z * b.x - a.x * b.z, 2.0) + 
+						powf(a.x * b.y - a.y * b.x, 2.0)
+						);
+				float numf = ppmsq * s;
+				int num = numf;
+				if(numf - num > ud(engine)) num ++;
+				for(int i = 0; i < num; i ++)
+				{
+					float r0 = ud(engine);
+					float r1 = ud(engine);
+					if(r0 + r1 > 1.0)
+					{
+						r0 = 1.0 - r0;
+						r1 = 1.0 - r1;
+					}
+					pcl::PointXYZ p = p0 + (a * r0 + b * r1);
+					pc_rs->points.push_back(p);
+				}
 			}
 		}
 		pc_rs->width = pc_rs->points.size();
 		pc_rs->height = 1;
 		pc_rs->is_dense = true;
 
-		pcl::toROSMsg(*pc_rs, pc_msg);
+
+		// Down-sample
+		pcl::PointCloud<pcl::PointXYZ>::Ptr pc_ds(new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::VoxelGrid<pcl::PointXYZ> ds;
+		ds.setInputCloud(pc_rs);
+		ds.setLeafSize(downsample_grid, downsample_grid, downsample_grid);
+		ds.filter(*pc_ds);
+
+		pcl::toROSMsg(*pc_ds, pc_msg);
 		pc_msg.header.frame_id = frame_id;
 		pc_msg.header.stamp = ros::Time::now();
-		ROS_INFO("pointcloud (%d points) has been generated from %s with %d verticles", 
-				(int)pc_rs->size(),
+		ROS_INFO("pointcloud (%d points) has been generated from %d verticles", 
+				(int)pc_ds->size(),
 				file.c_str(), (int)pc->size());
 		return pc_msg;
 	}
