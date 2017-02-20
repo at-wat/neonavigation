dc577b3938cb89bc6693dabc0020813ceb5f4413
Atsushi Watanabe <atsushi.w.roboken@gmail.com>
2017-02-21 02:16:46 +0900

    track_odometry: refactors tf_projection test code (#19)

--- track_odometry/src/tf_projection.cpp
+++ track_odometry/src/tf_projection.cpp
@@ -1,4 +1,69 @@
+#include <ros/ros.h>
+
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
 #include "tf_projection.hpp"
+
+class tf_projection_node : public tf_projection
+{
+private:
+	ros::NodeHandle nh;
+	tf::TransformBroadcaster tf_broadcaster;
+	tf::TransformListener tf_listener;
+
+	double rate;
+
+public:
+	tf_projection_node() :
+		nh("~")
+	{
+		nh.param("base_link_frame", frames["base"], std::string("base_link"));
+		nh.param("projection_frame", frames["projection"], std::string("map"));
+		nh.param("target_frame", frames["target"], std::string("map"));
+		nh.param("frame", frames["frame"], std::string("base_link_projected"));
+
+		nh.param("hz", rate, 10.0);
+	}
+	void process()
+	{
+		try
+		{
+			tf::StampedTransform trans;
+			tf_listener.waitForTransform(frames["projection"], frames["base"],
+					ros::Time(0), ros::Duration(0.1));
+			tf_listener.lookupTransform(frames["projection"], frames["base"],
+					ros::Time(0), trans);
+			
+			tf::StampedTransform trans_target;
+			tf_listener.waitForTransform(frames["target"], frames["projection"],
+					trans.stamp_, ros::Duration(0.1));
+			tf_listener.lookupTransform(frames["target"], frames["projection"],
+					trans.stamp_, trans_target);
+
+			const auto result = project(trans, trans_target);
+
+			geometry_msgs::TransformStamped trans_out;
+			tf::transformStampedTFToMsg(result, trans_out);
+
+			tf_broadcaster.sendTransform(trans_out);
+		}
+		catch(tf::TransformException &e)
+		{
+			ROS_WARN_ONCE("%s", e.what());
+		}
+	}
+	void spin()
+	{
+		ros::Rate r(rate);
+		while(ros::ok())
+		{
+			ros::spinOnce();
+			process();
+			r.sleep();
+		}
+	}
+};
 
 int main(int argc, char *argv[])
 {
