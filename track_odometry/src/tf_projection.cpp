b8412bd2aba0ee433ea651ce05b49756a61a70c7
Atsushi Watanabe <atsushi.w.roboken@gmail.com>
2017-02-23 23:10:03 +0900

    track_odometry: tf_projection: adds parameter to add tf timestamp offset (#21)

--- track_odometry/src/tf_projection.cpp
+++ track_odometry/src/tf_projection.cpp
@@ -13,6 +13,7 @@ private:
 	tf::TransformListener tf_listener;
 
 	double rate;
+	double tf_tolerance;
 	bool flat;
 
 public:
@@ -25,6 +26,7 @@ public:
 		nh.param("frame", frames["frame"], std::string("base_link_projected"));
 
 		nh.param("hz", rate, 10.0);
+		nh.param("tf_tolerance", tf_tolerance, 0.1);
 		nh.param("flat", flat, false);
 	}
 	void process()
@@ -52,6 +54,7 @@ public:
 				const float yaw = tf::getYaw(trans_out.transform.rotation);
 				trans_out.transform.rotation = tf::createQuaternionMsgFromYaw(yaw);
 			}
+			trans_out.header.stamp = trans.stamp_ + ros::Duration(tf_tolerance);
 
 			tf_broadcaster.sendTransform(trans_out);
 		}
