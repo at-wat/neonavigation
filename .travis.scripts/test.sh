c70b028458cbf1eeae2aef55f07575ddeb8fb554
Atsushi Watanabe <atsushi.w@ieee.org>
2017-08-17 14:49:49 +0900

    Add test result comment bot.

--- .travis.scripts/test.sh
+++ .travis.scripts/test.sh
@@ -3,6 +3,9 @@
 set -o errexit
 set -o verbose
 
+wget -q -P /tmp https://raw.githubusercontent.com/at-wat/gh-pr-comment/master/gh-pr-comment.sh
+source /tmp/gh-pr-comment.sh
+
 source /opt/ros/${ROS_DISTRO}/setup.bash
 source /catkin_ws/devel/setup.bash
 
@@ -14,9 +17,19 @@ apt-get clean && rm -rf /var/lib/apt/lists/*
 
 sync
 
-catkin_make && catkin_make tests && catkin_make run_tests
+catkin_make || (gh-pr-comment FAILED '```catkin_make``` failed'; false)
+catkin_make tests || (gh-pr-comment FAILED '```catkin_make tests``` failed'; false)
+catkin_make run_tests || (gh-pr-comment FAILED '```catkin_make run_tests``` failed'; false)
 
-catkin_test_results
+result_text="
+\`\`\`
+`catkin_test_results --all || true`
+\`\`\`
+`find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;'`
+"
+catkin_test_results || (gh-pr-comment FAILED "Test failed$result_text"; false)
+
+gh-pr-comment PASSED "All tests passed$result_text"
 
 cd ..
 rm -rf /catkin_ws || true
