e5ee7ae1d542622b11971adf923a36a61b1a2bd2
Atsushi Watanabe <atsushi.w@ieee.org>
2017-08-17 17:32:07 +0900

    Add test result comment bot. (#63)

--- .travis.scripts/test.sh
+++ .travis.scripts/test.sh
@@ -3,20 +3,43 @@
 set -o errexit
 set -o verbose
 
+wget -q -P /tmp https://raw.githubusercontent.com/at-wat/gh-pr-comment/master/gh-pr-comment.sh
+source /tmp/gh-pr-comment.sh
+
 source /opt/ros/${ROS_DISTRO}/setup.bash
 source /catkin_ws/devel/setup.bash
 
 cd /catkin_ws
 
 apt-get -qq update && \
+apt-get install libxml2-utils && \
 rosdep install --from-paths src/neonavigation --ignore-src --rosdistro=${ROS_DISTRO} -y && \
 apt-get clean && rm -rf /var/lib/apt/lists/*
 
 sync
 
-catkin_make && catkin_make tests && catkin_make run_tests
+catkin_make || (gh-pr-comment FAILED '```catkin_make``` failed'; false)
+catkin_make tests || (gh-pr-comment FAILED '```catkin_make tests``` failed'; false)
+catkin_make run_tests || (gh-pr-comment FAILED '```catkin_make run_tests``` failed'; false)
 
-catkin_test_results
+if [ catkin_test_results ];
+then
+  result_text="
+\`\`\`
+`catkin_test_results --all || true`
+\`\`\`
+"
+else
+  result_text="
+\`\`\`
+`catkin_test_results --all || true`
+\`\`\`
+`find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;'`
+"
+fi
+catkin_test_results || (gh-pr-comment FAILED "Test failed$result_text"; false)
+
+gh-pr-comment PASSED "All tests passed$result_text"
 
 cd ..
 rm -rf /catkin_ws || true
--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,21 @@
 FROM ros:kinetic
 
 RUN apt-get -qq update && \
-	apt-get install -y --no-install-recommends sudo libavcodec-dev libavutil-dev libgstreamer1.0 libqt5gui5 libqtgui4 libyaml-cpp-dev libpcl-dev python-vtk6 qt5-qmake qtbase5-dev qtbase5-dev-tools qtchooser qtcore4-l10n qttools5-dev qttools5-private-dev tcl-dev tk-dev vtk6 wget && \
+	apt-get install -y --no-install-recommends sudo libavcodec-dev libavutil-dev libgstreamer1.0 libqt5gui5 libqtgui4 libyaml-cpp-dev libpcl-dev python-vtk6 qt5-qmake qtbase5-dev qtbase5-dev-tools qtchooser qtcore4-l10n qttools5-dev qttools5-private-dev tcl-dev tk-dev vtk6 wget curl && \
 	apt-get clean && \
 	rm -rf /var/lib/apt/lists/*
 
 RUN rosdep update && \
 	mkdir -p /catkin_ws/src && \
 	bash -c "cd /catkin_ws/src && . /opt/ros/${ROS_DISTRO}/setup.bash && catkin_init_workspace && cd .. && catkin_make"
+
+ARG TRAVIS_PULL_REQUEST=false
+ARG TRAVIS_REPO_SLUG=""
+ARG TRAVIS_BOT_GITHUB_TOKEN=""
+
+ENV TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST
+ENV TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG
+ENV TRAVIS_BOT_GITHUB_TOKEN=$TRAVIS_BOT_GITHUB_TOKEN
 
 COPY ./ /catkin_ws/src/neonavigation
 RUN /catkin_ws/src/neonavigation/.travis.scripts/test.sh
