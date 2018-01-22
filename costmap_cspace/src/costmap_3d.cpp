7e14986c3e337025f67b21fbc728ed3f1b7a8b42
Atsushi Watanabe <atsushi.w@ieee.org>
2018-01-22 13:02:26 +0900

    Remove spin function

--- costmap_cspace/src/costmap_3d.cpp
+++ costmap_cspace/src/costmap_3d.cpp
@@ -122,10 +122,6 @@ protected:
   }
 
 public:
-  void spin()
-  {
-    ros::spin();
-  }
   Costmap3DOFNode()
     : nh_()
     , nhp_("~")
@@ -203,7 +199,7 @@ int main(int argc, char *argv[])
   ros::init(argc, argv, "costmap_3d");
 
   Costmap3DOFNode cm;
-  cm.spin();
+  ros::spin();
 
   return 0;
 }
