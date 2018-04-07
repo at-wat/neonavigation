b817342a23aaae50734f85c822f800edd2583fe1
Atsushi Watanabe <atsushi.w@ieee.org>
2018-04-08 01:17:47 +0900

    Save cache only on master and remove docker strage driver setup

--- .travis.scripts/save_cache.sh
+++ .travis.scripts/save_cache.sh
@@ -3,9 +3,9 @@
 set -o errexit
 set -o verbose
 
-#if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
-#then
+if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
+then
   docker tag docker-neonavigation:latest atwat/ros-neonavigation-cache:latest
   docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_TOKEN}
 	docker push atwat/ros-neonavigation-cache
-#fi
+fi
