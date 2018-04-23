ec81e170418dc782225d9c421ba7d66f4e14e70e
Atsushi Watanabe <atsushi.w@ieee.org>
2018-04-23 21:05:52 +0900

    joystick_interrupt: fix coding styles (https://github.com/at-wat/joystick_interrupt/issues/6)

--- src/joystick_interrupt.cpp
+++ src/joystick_interrupt.cpp
@@ -1,107 +1,135 @@
+/*
+ * Copyright (c) 2015-2018, the joystick_interrupt authors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 #include <std_msgs/Bool.h>
 
-
-class joystick_interrupt
+class JoystickInterrupt
 {
 private:
-	ros::NodeHandle nh;
-	ros::Subscriber sub_twist;
-	ros::Subscriber sub_joy;
-	ros::Publisher pub_twist;
-	ros::Publisher pub_int;
-	double linear_vel;
-	double angular_vel;
-	double timeout;
-	int linear_axis;
-	int angular_axis;
-	int linear_axis2;
-	int angular_axis2;
-	int interrupt_button;
-	ros::Time last_joy_msg;
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Subscriber sub_twist_;
+  ros::Subscriber sub_joy_;
+  ros::Publisher pub_twist_;
+  ros::Publisher pub_int_;
+  double linear_vel_;
+  double angular_vel_;
+  double timeout_;
+  int linear_axis_;
+  int angular_axis_;
+  int linear_axis2_;
+  int angular_axis2_;
+  int interrupt_button_;
+  ros::Time last_joy_msg_;
 
-	void cb_joy(const sensor_msgs::Joy::Ptr msg)
-	{
-		if(msg->buttons[interrupt_button])
-		{
-			last_joy_msg = ros::Time::now();
+  void cbJoy(const sensor_msgs::Joy::Ptr msg)
+  {
+    if (msg->buttons[interrupt_button_])
+    {
+      last_joy_msg_ = ros::Time::now();
 
-			float lin, ang;
-			lin = msg->axes[linear_axis];
-			ang = msg->axes[angular_axis];
-			if(linear_axis2 >= 0)
-			{
-				if(fabs(msg->axes[linear_axis2]) > fabs(lin))
-				{
-					lin = msg->axes[linear_axis2];
-				}
-			}
-			if(angular_axis2 >= 0)
-			{
-				if(fabs(msg->axes[angular_axis2]) > fabs(lin))
-				{
-					ang = msg->axes[angular_axis2];
-				}
-			}
+      float lin, ang;
+      lin = msg->axes[linear_axis_];
+      ang = msg->axes[angular_axis_];
+      if (linear_axis2_ >= 0)
+      {
+        if (fabs(msg->axes[linear_axis2_]) > fabs(lin))
+        {
+          lin = msg->axes[linear_axis2_];
+        }
+      }
+      if (angular_axis2_ >= 0)
+      {
+        if (fabs(msg->axes[angular_axis2_]) > fabs(lin))
+        {
+          ang = msg->axes[angular_axis2_];
+        }
+      }
 
-			geometry_msgs::Twist cmd_vel;
-			cmd_vel.linear.x = lin * linear_vel;
-			cmd_vel.linear.y = cmd_vel.linear.z = 0.0;
-			cmd_vel.angular.z = ang * angular_vel;
-			cmd_vel.angular.x = cmd_vel.angular.y = 0.0;
-			pub_twist.publish(cmd_vel);
-		}
-		else
-		{
-			last_joy_msg = ros::Time(0);
-		}
-	};
-	void cb_twist(const geometry_msgs::Twist::Ptr msg)
-	{
-		std_msgs::Bool status;
-		if(ros::Time::now() - last_joy_msg > ros::Duration(timeout))
-		{
-			pub_twist.publish(*msg);
-			status.data = true;
-		}
-		else
-		{
-			status.data = false;
-		}
-		pub_int.publish(status);
-	};
+      geometry_msgs::Twist cmd_vel;
+      cmd_vel.linear.x = lin * linear_vel_;
+      cmd_vel.linear.y = cmd_vel.linear.z = 0.0;
+      cmd_vel.angular.z = ang * angular_vel_;
+      cmd_vel.angular.x = cmd_vel.angular.y = 0.0;
+      pub_twist_.publish(cmd_vel);
+    }
+    else
+    {
+      last_joy_msg_ = ros::Time(0);
+    }
+  };
+  void cbTwist(const geometry_msgs::Twist::Ptr msg)
+  {
+    std_msgs::Bool status;
+    if (ros::Time::now() - last_joy_msg_ > ros::Duration(timeout_))
+    {
+      pub_twist_.publish(*msg);
+      status.data = true;
+    }
+    else
+    {
+      status.data = false;
+    }
+    pub_int_.publish(status);
+  };
 
 public:
-	joystick_interrupt():
-		nh("~")
-	{
-		sub_joy = nh.subscribe("/joy", 1, &joystick_interrupt::cb_joy, this);
-		sub_twist = nh.subscribe("cmd_vel_input", 1, &joystick_interrupt::cb_twist, this);
-		pub_twist = nh.advertise<geometry_msgs::Twist>("cmd_vel", 2);
-		pub_int = nh.advertise<std_msgs::Bool>("interrupt_status", 2);
+  JoystickInterrupt()
+    : nh_("")
+    , pnh_("~")
+  {
+    sub_joy_ = nh_.subscribe("joy", 1, &JoystickInterrupt::cbJoy, this);
+    sub_twist_ = pnh_.subscribe("cmd_vel_input", 1, &JoystickInterrupt::cbTwist, this);
+    pub_twist_ = pnh_.advertise<geometry_msgs::Twist>("cmd_vel", 2);
+    pub_int_ = pnh_.advertise<std_msgs::Bool>("interrupt_status", 2);
 
-		nh.param("linear_vel", linear_vel, 0.5);
-		nh.param("angular_vel", angular_vel, 0.8);
-		nh.param("linear_axis", linear_axis, 1);
-		nh.param("angular_axis", angular_axis, 0);
-		nh.param("linear_axis2", linear_axis2, -1);
-		nh.param("angular_axis2", angular_axis2, -1);
-		nh.param("interrupt_button", interrupt_button, 6);
-		nh.param("timeout", timeout, 0.5);
-		last_joy_msg = ros::Time(0);
-	}
+    pnh_.param("linear_vel", linear_vel_, 0.5);
+    pnh_.param("angular_vel", angular_vel_, 0.8);
+    pnh_.param("linear_axis", linear_axis_, 1);
+    pnh_.param("angular_axis", angular_axis_, 0);
+    pnh_.param("linear_axis2", linear_axis2_, -1);
+    pnh_.param("angular_axis2", angular_axis2_, -1);
+    pnh_.param("interrupt_button", interrupt_button_, 6);
+    pnh_.param("timeout", timeout_, 0.5);
+    last_joy_msg_ = ros::Time(0);
+  }
 };
 
 int main(int argc, char *argv[])
 {
-	ros::init(argc, argv, "joystick_interrupt");
-	
-	joystick_interrupt jy;
-	ros::spin();
+  ros::init(argc, argv, "joystick_interrupt");
 
-	return 0;
+  JoystickInterrupt jy;
+  ros::spin();
+
+  return 0;
 }
-
-
--- src/joystick_mux.cpp
+++ src/joystick_mux.cpp
@@ -1,86 +1,115 @@
+/*
+ * Copyright (c) 2015-2018, the joystick_interrupt authors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 
 #include <topic_tools/shape_shifter.h>
 
-class joystick_mux
+class JoystickMux
 {
 private:
-	ros::NodeHandle nh;
-	ros::Subscriber sub_topics[2];
-	ros::Subscriber sub_joy;
-	ros::Publisher pub_topic;
-	double timeout;
-	int interrupt_button;
-	ros::Time last_joy_msg;
-	bool advertised;
-	int selected;
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Subscriber sub_topics_[2];
+  ros::Subscriber sub_joy_;
+  ros::Publisher pub_topic_;
+  double timeout_;
+  int interrupt_button_;
+  ros::Time last_joy_msg_;
+  bool advertised_;
+  int selected_;
 
-	void cb_joy(const sensor_msgs::Joy::Ptr msg)
-	{
-		last_joy_msg = ros::Time::now();
-		if(msg->buttons[interrupt_button])
-		{
-			selected = 1;
-		}
-		else
-		{
-			selected = 0;
-		}
-	};
-	void cb_topic(const boost::shared_ptr<topic_tools::ShapeShifter const>& msg, int id)
-	{
-		if(selected == id)
-		{
-			if(!advertised)
-			{
-				advertised = true;
-				pub_topic = msg->advertise(nh, "output", 1, false);
-			}
-			pub_topic.publish(*msg);
-		}
-	};
+  void cbJoy(const sensor_msgs::Joy::Ptr msg)
+  {
+    last_joy_msg_ = ros::Time::now();
+    if (msg->buttons[interrupt_button_])
+    {
+      selected_ = 1;
+    }
+    else
+    {
+      selected_ = 0;
+    }
+  };
+  void cbTopic(const boost::shared_ptr<topic_tools::ShapeShifter const> &msg, int id)
+  {
+    if (selected_ == id)
+    {
+      if (!advertised_)
+      {
+        advertised_ = true;
+        pub_topic_ = msg->advertise(pnh_, "output", 1, false);
+      }
+      pub_topic_.publish(*msg);
+    }
+  };
 
 public:
-	joystick_mux():
-		nh("~")
-	{
-		sub_joy = nh.subscribe("/joy", 1, &joystick_mux::cb_joy, this);
-		sub_topics[0] = nh.subscribe<topic_tools::ShapeShifter>("input0", 1, 
-				boost::bind(&joystick_mux::cb_topic, this, _1, 0));
-		sub_topics[1] = nh.subscribe<topic_tools::ShapeShifter>("input1", 1, 
-				boost::bind(&joystick_mux::cb_topic, this, _1, 1));
+  JoystickMux()
+    : nh_("")
+    , pnh_("~")
+  {
+    sub_joy_ = nh_.subscribe("joy", 1, &JoystickMux::cbJoy, this);
+    sub_topics_[0] = pnh_.subscribe<topic_tools::ShapeShifter>(
+        "input0", 1, boost::bind(&JoystickMux::cbTopic, this, _1, 0));
+    sub_topics_[1] = pnh_.subscribe<topic_tools::ShapeShifter>(
+        "input1", 1, boost::bind(&JoystickMux::cbTopic, this, _1, 1));
 
-		nh.param("interrupt_button", interrupt_button, 5);
-		nh.param("timeout", timeout, 0.5);
-		last_joy_msg = ros::Time::now();
+    pnh_.param("interrupt_button", interrupt_button_, 5);
+    pnh_.param("timeout", timeout_, 0.5);
+    last_joy_msg_ = ros::Time::now();
 
-		advertised = false;
-		selected = 0;
-	}
-	void spin()
-	{
-		ros::Rate wait(10);
-		while(ros::ok())
-		{
-			wait.sleep();
-			ros::spinOnce();
-			if(ros::Time::now() - last_joy_msg > ros::Duration(timeout))
-			{
-				selected = 0;
-			}
-		}
-	}
+    advertised_ = false;
+    selected_ = 0;
+  }
+  void spin()
+  {
+    ros::Rate wait(10);
+    while (ros::ok())
+    {
+      wait.sleep();
+      ros::spinOnce();
+      if (ros::Time::now() - last_joy_msg_ > ros::Duration(timeout_))
+      {
+        selected_ = 0;
+      }
+    }
+  }
 };
 
 int main(int argc, char *argv[])
 {
-	ros::init(argc, argv, "joystick_mux");
-	
-	joystick_mux jy;
-	jy.spin();
+  ros::init(argc, argv, "joystick_mux");
 
-	return 0;
+  JoystickMux jy;
+  jy.spin();
+
+  return 0;
 }
-
-
