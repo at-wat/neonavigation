c84fd810ebf78fbc5b4525b4ee58e82ef7d4a238
Atsushi Watanabe <atsushi.w@ieee.org>
2018-05-16 16:39:49 +0900

    Fix include path

--- costmap_cspace/include/costmap_cspace/costmap_3d_layer/stop_propagation.h
+++ costmap_cspace/include/costmap_cspace/costmap_3d_layer/stop_propagation.h
@@ -27,15 +27,15 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef COSTMAP_3D_LAYER_STOP_PROPAGATION_H
-#define COSTMAP_3D_LAYER_STOP_PROPAGATION_H
+#ifndef COSTMAP_CSPACE_COSTMAP_3D_LAYER_STOP_PROPAGATION_H
+#define COSTMAP_CSPACE_COSTMAP_3D_LAYER_STOP_PROPAGATION_H
 
 #include <geometry_msgs/PolygonStamped.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <costmap_cspace/CSpace3D.h>
 #include <costmap_cspace/CSpace3DUpdate.h>
 
-#include <costmap_3d_layer/base.h>
+#include <costmap_cspace/costmap_3d_layer/base.h>
 
 namespace costmap_cspace
 {
@@ -72,4 +72,4 @@ protected:
 };
 }  // namespace costmap_cspace
 
-#endif  // COSTMAP_3D_LAYER_STOP_PROPAGATION_H
+#endif  // COSTMAP_CSPACE_COSTMAP_3D_LAYER_STOP_PROPAGATION_H
