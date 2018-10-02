fb1f4e4a00e1375e434f1278f6f1921007045d1e
Yuta Koga <ykoga.kyutech@gmail.com>
2018-10-02 11:26:28 +0900

    Add diagnostics to planner node

--- planner_cspace/src/planner_3d.cpp
+++ planner_cspace/src/planner_3d.cpp
@@ -31,6 +31,7 @@
 
 #include <costmap_cspace_msgs/CSpace3D.h>
 #include <costmap_cspace_msgs/CSpace3DUpdate.h>
+#include <diagnostic_updater/diagnostic_updater.h>
 #include <nav_msgs/GetPlan.h>
 #include <nav_msgs/Path.h>
 #include <planner_cspace_msgs/PlannerStatus.h>
@@ -219,6 +220,8 @@ protected:
 
   MotionCache<Astar::Vec, Astar::Vecf>::Ptr motion_cache_;
   MotionCache<Astar::Vec, Astar::Vecf>::Ptr motion_cache_linear_;
+
+  diagnostic_updater::Updater diag_updater_;
 
   bool cbForget(std_srvs::EmptyRequest &req,
                 std_srvs::EmptyResponse &res)
@@ -1243,6 +1246,9 @@ public:
     escaping_ = false;
     cnt_stuck_ = 0;
 
+    diag_updater_.setHardwareID("none");
+    diag_updater_.add("Status", this, &Planner3dNode::diagnoseStatus);
+
     act_->start();
   }
   void spin()
@@ -1342,6 +1348,7 @@ public:
         pub_path_.publish(path);
       }
       pub_status_.publish(status_);
+      diag_updater_.force_update();
     }
   }
 
@@ -1781,6 +1788,24 @@ protected:
     }
 
     return cost;
+  }
+
+  void diagnoseStatus(diagnostic_updater::DiagnosticStatusWrapper &stat)
+  {
+    if (status_.error == planner_cspace_msgs::PlannerStatus::IN_ROCK)
+    {
+      stat.summary(diagnostic_msgs::DiagnosticStatus::ERROR, "The robot is in rock.");
+    }
+    else if (status_.error == planner_cspace_msgs::PlannerStatus::PATH_NOT_FOUND)
+    {
+      stat.summary(diagnostic_msgs::DiagnosticStatus::ERROR, "Path not found.");
+    }
+    else
+    {
+      stat.summary(diagnostic_msgs::DiagnosticStatus::OK, "Going well.");
+    }
+    stat.addf("status", "%u", status_.status);
+    stat.addf("error", "%u", status_.error);
   }
 };
 
