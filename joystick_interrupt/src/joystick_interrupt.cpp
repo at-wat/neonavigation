b34cbf04f0bf5266f8073e5c6008c8d0dbee3b25
Atsushi Watanabe <atsushi.w@ieee.org>
2019-01-13 15:18:56 +0900

    Add LICENSE file (#270)
    * Update copyright notice (change copyright holder, which I have all rights, to neonavigation authors)
    
    * Add LICENSE file

--- joystick_interrupt/src/joystick_interrupt.cpp
+++ joystick_interrupt/src/joystick_interrupt.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2018, the joystick_interrupt authors
+ * Copyright (c) 2015-2018, the neonavigation authors
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
