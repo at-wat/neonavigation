ae9a7031fc6a37fd1059bfb308e1f30ec546c2a8
Atsushi Watanabe <atsushi.w@ieee.org>
2019-04-03 23:37:59 +0900

    Update CI script

--- .travis/test.sh
+++ .travis/test.sh
@@ -11,14 +11,15 @@ set -o verbose
 
 cd /catkin_ws
 
+build_number="[[#${TRAVIS_BUILD_NUMBER}](${TRAVIS_BUILD_WEB_URL})]"
 
-pkgs=`find . -name package.xml | xargs -n1 dirname`
+pkgs=$(find . -name package.xml | xargs -n1 dirname)
 catkin_lint $pkgs \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" \
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" \
       "<details><summary>catkin_lint failed</summary>
 
 \`\`\`
-`catkin_lint $pkgs 2>&1`
+$(catkin_lint $pkgs 2>&1)
 \`\`\`
 </details>"; false)
 
@@ -62,15 +63,18 @@ sed -i -e "5a set(CMAKE_CXX_FLAGS \"-Wall -Werror -O2 ${COVERAGE_OPTION}\")" \
 CM_OPTIONS=""
 
 catkin_make ${CM_OPTIONS} \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
 catkin_make tests ${CM_OPTIONS} \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
 catkin_make run_tests ${CM_OPTIONS} --make-args -j1 -l1 \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
 
 if [ x${COVERAGE_TEST} == "xtrue" ]
 then
-  (cd src/neonavigation/; cp -r /catkin_ws/build ./; bash <(curl -s https://codecov.io/bash) -y .codecov.yml)
+  cd src/neonavigation/
+  cp -r /catkin_ws/build ./
+  rm -rf neonavigation_rviz_plugins neonavigation_msgs
+  bash <(curl -s https://codecov.io/bash) -y .codecov.yml --silent
   exit 0
 fi
 
@@ -78,21 +82,22 @@ if [ catkin_test_results ];
 then
   result_text="
 \`\`\`
-`catkin_test_results --all | grep -v Skipping || true`
+$(catkin_test_results --all | grep -v Skipping || true)
 \`\`\`
 "
 else
   result_text="
 \`\`\`
-`catkin_test_results --all | grep -v Skipping || true`
+$(catkin_test_results --all | grep -v Skipping || true)
 \`\`\`
-`find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;'`
+$(find build/test_results/ -name *.xml \
+  | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;')
 "
 fi
-catkin_test_results || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" "<details><summary>Test failed</summary>
+catkin_test_results || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" "<details><summary>Test failed</summary>
 
 $result_text</details>"; false)
 
-gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] PASSED on ${ROS_DISTRO}" "<details><summary>All tests passed</summary>
+gh-pr-comment "${build_number} PASSED on ${ROS_DISTRO}" "<details><summary>All tests passed</summary>
 
 $result_text</details>" || true
