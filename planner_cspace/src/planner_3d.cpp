88e1479cb39d1a29fe048907c1564b0bcece3145
Atsushi Watanabe <atsushi.w@ieee.org>
2019-05-16 12:59:37 +0900

    Fix empty path publish (#301)

--- planner_cspace/src/planner_3d.cpp
+++ planner_cspace/src/planner_3d.cpp
@@ -687,7 +687,16 @@ protected:
     nav_msgs::Path path;
     path.header.frame_id = robot_frame_;
     path.header.stamp = ros::Time::now();
-    pub_path_.publish(path);
+    if (use_path_with_velocity_)
+    {
+      pub_path_velocity_.publish(
+          trajectory_tracker_msgs::toPathWithVelocity(
+              path, std::numeric_limits<double>::quiet_NaN()));
+    }
+    else
+    {
+      pub_path_.publish(path);
+    }
   }
 
   void cbMapUpdate(const costmap_cspace_msgs::CSpace3DUpdate::ConstPtr& msg)
