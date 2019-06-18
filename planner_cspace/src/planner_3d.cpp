fe05ff84e7593d55e8eadc718e9b32c6a8dc3409
Daiki Maekawa <method_aspect_card@yahoo.co.jp>
2019-06-18 15:22:48 +0900

    planner_cspace: reject request if input frame are located at diffrent frame to the map (#327)

--- planner_cspace/src/planner_3d.cpp
+++ planner_cspace/src/planner_3d.cpp
@@ -39,9 +39,9 @@
 #include <costmap_cspace_msgs/CSpace3D.h>
 #include <costmap_cspace_msgs/CSpace3DUpdate.h>
 #include <diagnostic_updater/diagnostic_updater.h>
+#include <geometry_msgs/PoseArray.h>
 #include <nav_msgs/GetPlan.h>
 #include <nav_msgs/Path.h>
-#include <geometry_msgs/PoseArray.h>
 #include <planner_cspace_msgs/PlannerStatus.h>
 #include <sensor_msgs/PointCloud.h>
 #include <std_srvs/Empty.h>
@@ -50,8 +50,8 @@
 
 #include <ros/console.h>
 #include <tf2/utils.h>
-#include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <actionlib/server/simple_action_server.h>
 #include <move_base_msgs/MoveBaseAction.h>
@@ -63,8 +63,8 @@
 #include <planner_cspace/planner_3d/grid_metric_converter.h>
 #include <planner_cspace/planner_3d/jump_detector.h>
 #include <planner_cspace/planner_3d/motion_cache.h>
-#include <planner_cspace/planner_3d/rotation_cache.h>
 #include <planner_cspace/planner_3d/path_interpolator.h>
+#include <planner_cspace/planner_3d/rotation_cache.h>
 
 #include <omp.h>
 
@@ -252,6 +252,16 @@ protected:
     if (!has_map_)
       return false;
 
+    if (req.start.header.frame_id != map_header_.frame_id ||
+        req.goal.header.frame_id != map_header_.frame_id)
+    {
+      ROS_ERROR("Start [%s] and Goal [%s] poses must be in the map frame [%s].",
+                req.start.header.frame_id.c_str(),
+                req.goal.header.frame_id.c_str(),
+                map_header_.frame_id.c_str());
+      return false;
+    }
+
     Astar::Vec s, e;
     grid_metric_converter::metric2Grid(
         map_info_, s[0], s[1], s[2],
@@ -376,6 +386,13 @@ protected:
   }
   bool setGoal(const geometry_msgs::PoseStamped& msg)
   {
+    if (msg.header.frame_id != map_header_.frame_id)
+    {
+      ROS_ERROR("Goal [%s] pose must be in the map frame [%s].",
+                msg.header.frame_id.c_str(), map_header_.frame_id.c_str());
+      return false;
+    }
+
     goal_raw_ = goal_ = msg;
 
     const double len2 =
