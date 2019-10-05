ff35b45170cd1ef0745e4457eb7240104bceb0ed
Atsushi Watanabe <atsushi.w@ieee.org>
2019-10-05 18:43:59 +0900

    Fix addressing on updated map

--- costmap_cspace/include/costmap_cspace/costmap_3d_layer/footprint.h
+++ costmap_cspace/include/costmap_cspace/costmap_3d_layer/footprint.h
@@ -266,8 +266,11 @@ protected:
 
             if (keep_unknown_)
             {
-              const size_t i2 = y2 * map->info.width + x2;
-              if (msg->data[i2] < 0)
+              const int mx = lroundf((gx - ox) * map->info.linear_resolution / msg->info.resolution);
+              const int my = lroundf((gy - oy) * map->info.linear_resolution / msg->info.resolution);
+
+              const size_t i2 = my * map->info.width + mx;
+              if (i2 < msg->data.size() && msg->data[i2] < 0)
                 continue;
             }
 
