47d934a059276500c6923aeda69c423bac021fc6
Atsushi Watanabe <atsushi.w@ieee.org>
2020-01-01 01:39:27 +0900

    Split cpp

--- planner_cspace/include/planner_cspace/planner_2dof_serial_joints/grid_astar_model.h
+++ planner_cspace/include/planner_cspace/planner_2dof_serial_joints/grid_astar_model.h
@@ -33,12 +33,7 @@
 #include <utility>
 #include <vector>
 
-#include <costmap_cspace_msgs/MapMetaData3D.h>
-
 #include <planner_cspace/cyclic_vec.h>
-#include <planner_cspace/planner_3d/motion_cache.h>
-#include <planner_cspace/planner_3d/rotation_cache.h>
-#include <planner_cspace/planner_3d/path_interpolator.h>
 #include <planner_cspace/grid_astar_model.h>
 #include <planner_cspace/blockmem_gridmap.h>
 
@@ -66,86 +61,21 @@ protected:
   int range_;
 
 public:
-  explicit GridAstarModel2DoFSerialJoint(
+  GridAstarModel2DoFSerialJoint(
       const Vecf& euclid_cost_coef,
       const int resolution,
       BlockMemGridmap<char, 2, 0, 0x40>& cm,
       const CostCoeff& cc,
-      const int range)
-    : euclid_cost_coef_(euclid_cost_coef)
-    , resolution_(resolution)
-    , cm_(cm)
-    , cc_(cc)
-    , range_(range)
-  {
-    for (Vec p(-range, -range); p[0] <= range; p[0]++)
-    {
-      for (p[1] = -range; p[1] <= range; p[1]++)
-      {
-        search_list_.push_back(p);
-      }
-    }
-  }
-  float euclidCost(const Vec& v) const
-  {
-    auto vc = v;
-    float cost = 0;
-    for (int i = 0; i < 2; i++)
-    {
-      cost += fabs(euclid_cost_coef_[i] * vc[i]);
-    }
-    return cost;
-  }
+      const int range);
+  float euclidCost(const Vec& v) const;
   float cost(
-      const Vec& cur, const Vec& next, const std::vector<VecWithCost>& start, const Vec& goal) const override
-  {
-    if ((unsigned int)next[0] >= (unsigned int)resolution_ * 2 ||
-        (unsigned int)next[1] >= (unsigned int)resolution_ * 2)
-      return -1;
-    Vec d = next - cur;
-    d.cycle(resolution_, resolution_);
-
-    float cost = euclidCost(d);
-
-    float distf = hypotf(static_cast<float>(d[0]), static_cast<float>(d[1]));
-    float v[2], dp[2];
-    int sum = 0;
-    const int dist = distf;
-    distf /= dist;
-    v[0] = cur[0];
-    v[1] = cur[1];
-    dp[0] = static_cast<float>(d[0]) / dist;
-    dp[1] = static_cast<float>(d[1]) / dist;
-    Vec pos;
-    for (int i = 0; i < dist; i++)
-    {
-      pos[0] = lroundf(v[0]);
-      pos[1] = lroundf(v[1]);
-      pos.cycleUnsigned(resolution_, resolution_);
-      const auto c = cm_[pos];
-      if (c > 99)
-        return -1;
-      sum += c;
-      v[0] += dp[0];
-      v[1] += dp[1];
-    }
-    cost += sum * cc_.weight_cost_ / 100.0;
-    return cost;
-  }
-
+      const Vec& cur, const Vec& next, const std::vector<VecWithCost>& start, const Vec& goal) const override;
   float costEstim(
-      const Vec& cur, const Vec& goal) const override
-  {
-    const Vec d = goal - cur;
-    return euclidCost(d);
-  }
+      const Vec& cur, const Vec& goal) const override;
   const std::vector<Vec>& searchGrids(
       const Vec& p,
       const std::vector<VecWithCost>& ss,
-      const Vec& es) const override
-  {
-    return search_list_;
-  }
+      const Vec& es) const override;
 };
 
 #endif  // PLANNER_CSPACE_PLANNER_2DOF_SERIAL_JOINTS_GRID_ASTAR_MODEL_H
