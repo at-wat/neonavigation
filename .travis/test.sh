28b97fb1a26ef2045c629007e028c2c6a1f20b40
Atsushi Watanabe <atsushi.w@ieee.org>
2020-01-02 18:28:52 +0900

    Update CI setting (#410)
    * Remove coverage-only build
    * Fix codecov setting

--- .travis/test.sh
+++ .travis/test.sh
@@ -21,34 +21,7 @@ $(catkin_lint $pkgs 2>&1)
 COVERAGE_OPTION=
 if [ x${COVERAGE_TEST} == "xtrue" ]
 then
-  # Workaround: Ubuntu Xenial uses quite old gcc with a bug.
-  #   https://gcc.gnu.org/bugzilla/show_bug.cgi?id=65831
-  #   Install newer gcc to get correct coverage
-  apt-get update -qq
-  if ! apt-cache search "^gcc-5$" | grep gcc-5; then
-    echo "gcc-5 not found. enabling ppa:ubuntu-toolchain-r/test"
-    apt-get install --no-install-recommends software-properties-common -y
-    add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    apt-get update -qq
-  fi
-  apt-get install --no-install-recommends gcc-5 g++-5 -y
-  update-alternatives \
-    --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 \
-    --slave /usr/bin/g++ g++ /usr/bin/g++-5 \
-    --slave /usr/bin/gcov gcov /usr/bin/gcov-5
-
   gcov -v
-
-  # Workaround: Ubuntu Xenial has too old Eigen3 which causes
-  #             deprecated warning on gcc-5 with C++11.
-  (cd /tmp \
-    && git clone -b 3.3.3 --depth=1 https://github.com/eigenteam/eigen-git-mirror.git eigen \
-    && mkdir eigen/build \
-    && cd eigen/build \
-    && cmake .. -DCMAKE_INSTALL_PREFIX=/usr \
-    && make install)
-  rm -rf /tmp/eigen
-
   COVERAGE_OPTION=-coverage
 fi
 
@@ -72,9 +45,8 @@ then
   cd src/neonavigation/
   cp -r /catkin_ws/build ./
   rm -rf build/neonavigation_rviz_plugins build/neonavigation_msgs
-  bash <(curl -s https://codecov.io/bash) -y .codecov.yml -Z \
+  bash <(curl -s https://codecov.io/bash) -y codecov.yml -Z \
     | grep -i -e error -e fail
-  exit 0
 fi
 
 if [ catkin_test_results ];
