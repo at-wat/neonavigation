17a6b0c6ec1b5af383b67c783a583a6233962624
Atsushi Watanabe <atsushi.w@ieee.org>
2020-01-16 20:19:13 +0900

    Run prerelease test with latest msgs packages (#423)

--- .travis/prerelease_test.sh
+++ .travis/prerelease_test.sh
@@ -43,6 +43,7 @@ generate_prerelease_script.py \
   ${ROS_DISTRO_TARGET} default ubuntu ${UBUNTU_DIST_TARGET} amd64 \
   --custom-repo \
     neonavigation__custom-2:git:https://github.com/at-wat/neonavigation.git:${TRAVIS_PULL_REQUEST_BRANCH} \
+    neonavigation_msgs__custom-2:git:https://github.com/at-wat/neonavigation_msgs.git:master \
   --level 1 \
   --output-dir ./
 
