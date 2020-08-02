399692e72b68355bb37173ec85e5f14d03755153
Atsushi Watanabe <atsushi.w@ieee.org>
2020-08-03 01:32:55 +0900

    Save cache only on master

--- .travis/save_cache.sh
+++ .travis/save_cache.sh
@@ -3,5 +3,8 @@
 set -o errexit
 set -o verbose
 
-layers=$(echo $@ | xargs -n1 docker history -q | grep -v '<missing>' | sort | uniq)
-docker save ${layers} -o ${HOME}/.cache/docker/image.tar
+if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]]
+then
+  layers=$(echo $@ | xargs -n1 docker history -q | grep -v '<missing>' | sort | uniq)
+  docker save ${layers} -o ${HOME}/.cache/docker/image.tar
+fi
