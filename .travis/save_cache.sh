36012ccf7f649bf7616f976375b91d597932746c
Atsushi Watanabe <atsushi.w@ieee.org>
2020-08-02 21:47:20 +0900

    Always save cache

--- .travis/save_cache.sh
+++ .travis/save_cache.sh
@@ -3,8 +3,5 @@
 set -o errexit
 set -o verbose
 
-if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
-then
-  layers=$(echo $@ | xargs -n1 docker history -q | grep -v '<missing>' | sort | uniq)
-  docker save ${layers} -o ${HOME}/.cache/docker/image.tar
-fi
+layers=$(echo $@ | xargs -n1 docker history -q | grep -v '<missing>' | sort | uniq)
+docker save ${layers} -o ${HOME}/.cache/docker/image.tar
