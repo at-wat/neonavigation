284ec860b445b6eeadbaddbdef62e8556b05281b
Naotaka Hatao <hatao.jsk@gmail.com>
2020-10-22 15:29:14 +0900

    Fix roslint error

--- costmap_cspace/include/costmap_cspace/costmap_3d_layer/footprint.h
+++ costmap_cspace/include/costmap_cspace/costmap_3d_layer/footprint.h
@@ -30,6 +30,7 @@
 #ifndef COSTMAP_CSPACE_COSTMAP_3D_LAYER_FOOTPRINT_H
 #define COSTMAP_CSPACE_COSTMAP_3D_LAYER_FOOTPRINT_H
 
+#include <algorithm>
 #include <cmath>
 #include <memory>
 #include <vector>
