be42807d0bce222ee235ed0106b6e48c47fcaa4f
Atsushi Watanabe <atsushi.w@ieee.org>
2021-09-30 19:25:58 +0900

    Update default num_cost_estim_task

--- planner_cspace/src/planner_3d.cpp
+++ planner_cspace/src/planner_3d.cpp
@@ -1332,7 +1332,7 @@ public:
     int num_task;
     pnh_.param("num_search_task", num_task, num_threads * 16);
     as_.setSearchTaskNum(num_task);
-    pnh_.param("num_cost_estim_task", num_cost_estim_task_, num_threads * 16);
+    pnh_.param("num_cost_estim_task", num_cost_estim_task_, num_threads * 64);
 
     pnh_.param("retain_last_error_status", retain_last_error_status_, true);
     status_.status = planner_cspace_msgs::PlannerStatus::DONE;
