2564c725df2cfc0f53bedb1db3644448bcc84d9d
Atsushi Watanabe <atsushi.w@ieee.org>
2023-02-24 22:02:24 +0900

    planner_cspace: fix map update range (#672)
    - Fix memory access violation
    - Fix map_update_max to contain upper boundary edge pixels
      Word definitions:
        - max: contains the value
        - size: does not contain the value
    
    Co-authored-by: Kazuki Takahashi <tkhsh.schoen@gmail.com>

--- planner_cspace/include/planner_cspace/blockmem_gridmap.h
+++ planner_cspace/include/planner_cspace/blockmem_gridmap.h
@@ -129,11 +129,11 @@ public:
       const T zero, const CyclicVecInt<DIM, NONCYCLIC>& min, const CyclicVecInt<DIM, NONCYCLIC>& max)
   {
     CyclicVecInt<DIM, NONCYCLIC> p = min;
-    for (p[0] = min[0]; p[0] < max[0]; ++p[0])
+    for (p[0] = min[0]; p[0] <= max[0]; ++p[0])
     {
-      for (p[1] = min[1]; p[1] < max[1]; ++p[1])
+      for (p[1] = min[1]; p[1] <= max[1]; ++p[1])
       {
-        for (p[2] = min[2]; p[2] < max[2]; ++p[2])
+        for (p[2] = min[2]; p[2] <= max[2]; ++p[2])
         {
           (*this)[p] = zero;
         }
@@ -145,11 +145,11 @@ public:
       const CyclicVecInt<DIM, NONCYCLIC>& max)
   {
     CyclicVecInt<DIM, NONCYCLIC> p = min;
-    for (p[0] = min[0]; p[0] < max[0]; ++p[0])
+    for (p[0] = min[0]; p[0] <= max[0]; ++p[0])
     {
-      for (p[1] = min[1]; p[1] < max[1]; ++p[1])
+      for (p[1] = min[1]; p[1] <= max[1]; ++p[1])
       {
-        for (p[2] = min[2]; p[2] < max[2]; ++p[2])
+        for (p[2] = min[2]; p[2] <= max[2]; ++p[2])
         {
           (*this)[p] = base[p];
         }
