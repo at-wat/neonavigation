3c6155ac7b04179debf99a36e55db2d4add75545
Atsushi Watanabe <atsushi.w@ieee.org>
2025-07-08 14:14:13 +0900

    Output planner error status based on escape reason

--- planner_cspace/test/src/test_navigate.cpp
+++ planner_cspace/test/src/test_navigate.cpp
@@ -752,9 +752,18 @@ TEST_F(Navigate, RobotIsInCrowd)
       continue;
     }
 
-    EXPECT_EQ(planner_status_->error, planner_cspace_msgs::PlannerStatus::GOING_WELL);
-
     const auto goal_rel = trans.inverse() * goal;
+
+    if (planner_status_->status == planner_cspace_msgs::PlannerStatus::DOING &&
+        goal_rel.getOrigin().length() > 0.8)
+    {
+      EXPECT_EQ(planner_status_->error, planner_cspace_msgs::PlannerStatus::PATH_NOT_FOUND);
+    }
+    else if (goal_rel.getOrigin().length() < 0.5)
+    {
+      EXPECT_EQ(planner_status_->error, planner_cspace_msgs::PlannerStatus::GOING_WELL);
+    }
+
     if (goal_rel.getOrigin().length() < 0.2 &&
         std::abs(tf2::getYaw(goal_rel.getRotation())) < 0.2 &&
         planner_status_->status == planner_cspace_msgs::PlannerStatus::DONE)
